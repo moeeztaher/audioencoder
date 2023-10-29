bbd57c0936ef28bb30f673a543a3ba9170b2d9aa
root <root@DESKTOP-2HPJ2UG>
2023-10-29 19:02:31 +0100

    first run 1 wavfile with logs

---
+++ CMakeFiles/encoder.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encoder.dir/src/encoder.cpp.o"
+  "CMakeFiles/encoder.dir/src/encoder.cpp.o.d"
+  "CMakeFiles/encoder.dir/src/fs.cpp.o"
+  "CMakeFiles/encoder.dir/src/fs.cpp.o.d"
+  "CMakeFiles/encoder.dir/src/lame_wrapper.cpp.o"
+  "CMakeFiles/encoder.dir/src/lame_wrapper.cpp.o.d"
+  "CMakeFiles/encoder.dir/src/log.cpp.o"
+  "CMakeFiles/encoder.dir/src/log.cpp.o.d"
+  "CMakeFiles/encoder.dir/src/main.cpp.o"
+  "CMakeFiles/encoder.dir/src/main.cpp.o.d"
+  "CMakeFiles/encoder.dir/src/wav.cpp.o"
+  "CMakeFiles/encoder.dir/src/wav.cpp.o.d"
+  "encoder"
+  "encoder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
