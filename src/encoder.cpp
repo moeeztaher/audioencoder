3287b0daf9e7d0552ec6b3ed0aaed94e2ca8a8ea
Moeez Tahir <moeztahir96@gmail.com>
2023-10-29 19:25:47 +0100

    removed multithread

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -3,13 +3,6 @@
 #include "lame_wrapper.h"
 #include "log.h"
 #include "wav.h"
-#include <iostream>
-#include <vector>
-#include <thread>
-#include <future>
-#include <cstdint>
-#include <filesystem>
-
 
 namespace
 {
@@ -17,69 +10,45 @@ namespace
     {
         cin::log::info("Encoding {}", path.string());
 
-        cin::WavFile wav_file{ path };
+        cin::WavFile wav_file{path};
 
         if (wav_file.num_channels() > 2) {
             throw cin::Encoder::UnsupportedFormat("More than two channels are not supported");
         }
 
-        std::filesystem::path output_path{ path };
+        std::filesystem::path output_path{path};
         output_path.replace_extension(".mp3");
-        std::ofstream mp3_file{ output_path, std::ios::binary };
+        std::ofstream mp3_file{output_path, std::ios::binary};
 
-        const int num_channels{ wav_file.num_channels() };
-        cin::Lame lame{ num_channels, wav_file.sample_rate() };
+        const int num_channels{wav_file.num_channels()};
+        cin::Lame lame{num_channels, wav_file.sample_rate()};
 
-        constexpr size_t num_samples{ 1024 };
-        constexpr size_t mp3_buffer_size{ static_cast<size_t>(num_samples * 1.25) + 7200 };
+        constexpr size_t num_samples{1024};
+        constexpr size_t mp3_buffer_size{static_cast<size_t>(num_samples * 1.25) + 7200};
         std::vector<uint8_t> mp3_buffer;
         mp3_buffer.reserve(mp3_buffer_size);
         std::vector<int16_t> sample_buffer;
 
-        const size_t num_frames{ num_samples / (num_channels == 1 ? 1 : 2) };
-        size_t read_size{ 0 };
-        size_t write_size{ 0 };
+        const size_t num_frames{num_samples / (num_channels == 1 ? 1 : 2)};
+        size_t read_size{0};
+        size_t write_size{0};
 
-        std::mutex buffer_mutex; // Mutex to protect mp3_buffer
+        while (true) {
+            read_size += wav_file.read_samples(sample_buffer, num_frames) * sizeof(uint16_t);
 
-        const int num_threads = std::thread::hardware_concurrency(); // Get the number of CPU cores
+            mp3_buffer.resize(mp3_buffer_size);
 
-        // Function to perform encoding on a chunk of samples
-        auto encode_chunk = [&wav_file, &lame, &mp3_buffer, &sample_buffer, &mp3_file, &read_size, &write_size, &buffer_mutex, num_frames]() {
-            while (true) {
-                std::vector<int16_t> local_sample_buffer;
-                local_sample_buffer.reserve(num_frames);
-
-                {
-                    std::lock_guard<std::mutex> lock(buffer_mutex);
-                    if (sample_buffer.empty()) {
-                        return; // No more work to do
-                    }
-
-                    read_size += wav_file.read_samples(local_sample_buffer, num_frames) * sizeof(uint16_t);
-                }
-
-                mp3_buffer.resize(mp3_buffer_size);
-                write_size += lame.encode(local_sample_buffer, mp3_buffer);
-
-                {
-                    std::lock_guard<std::mutex> lock(buffer_mutex);
-                    mp3_file.write((char*)mp3_buffer.data(), mp3_buffer.size());
-                }
+            if (sample_buffer.empty()) {
+                write_size += lame.flush(mp3_buffer);
+                mp3_file.write((char *) mp3_buffer.data(), mp3_buffer.size());
+                break;
             }
-            };
 
-        std::vector<std::thread> threads;
-
-        for (int i = 0; i < num_threads; ++i) {
-            threads.emplace_back(encode_chunk);
+            write_size += lame.encode(sample_buffer, mp3_buffer);
+            mp3_file.write((char *) mp3_buffer.data(), mp3_buffer.size());
         }
 
-        for (auto& thread : threads) {
-            thread.join();
-        }
-
-        constexpr float bytes_per_kib{ 1024.0F };
+        constexpr float bytes_per_kib{1024.0F};
 
         cin::log::debug(" size reduced from {:.2f} KiB to {:.2f} KiB ({:.2f}x smaller)",
             read_size / bytes_per_kib,
